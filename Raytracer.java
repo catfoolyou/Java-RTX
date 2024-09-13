198eafc1a561a2c6e7b1e23e2c83fca7013fe2fb
CatFoolYou <95507639+catfoolyou@users.noreply.github.com>
2024-09-13 12:00:14 -0400

    Demo scene + misc changes

--- Raytracer.java
+++ Raytracer.java
@@ -21,32 +21,60 @@ public class Raytracer{
         
         HittableList world = new HittableList();
 
-        Material material_ground = new Lambertian(new Vector3(0.8, 0.8, 0.0));
-        Material material_center = new Lambertian(new Vector3(0.1, 0.2, 0.5));
-        Material material_left = new Dielectric(1.50);
-        Material material_bubble = new Dielectric(1.00 / 1.50);
-        Material material_right = new Metal(new Vector3(0.8, 0.6, 0.2), 0.01);
+        Material ground_material = new Lambertian(new Vector3(0.5, 0.5, 0.5));
+        world.add(new Sphere(new Vector3(0,-1000,0), 1000, ground_material));
 
-        world.add(new Sphere(new Vector3( 0.0, -100.5, -1.0), 100.0, material_ground));
-        world.add(new Sphere(new Vector3( 0.0, 0.0, -1.2), 0.5, material_center));
-        world.add(new Sphere(new Vector3(-1.0, 0.0, -1.0), 0.5, material_left));
-        world.add(new Sphere(new Vector3(-1.0, 0.0, -1.0), 0.4, material_bubble));
-        world.add(new Sphere(new Vector3( 1.0,0.0, -1.0), 0.5, material_right));
+        for (int a = -11; a < 11; a++) {
+            for (int b = -11; b < 11; b++) {
+                double choose_mat = random_double();
+                Vector3 center = new Vector3(a + 0.9 * random_double(), 0.2, b + 0.9 * random_double());
+
+                if ((center.subtract(new Vector3(4, 0.2, 0)).length()) > 0.9) {
+                    Material sphere_material;
+
+                    if (choose_mat < 0.8) {
+                        // diffuse
+                        Vector3 albedo = new Vector3(Math.random(), Math.random(), Math.random());
+                        sphere_material = new Lambertian(albedo);
+                        world.add(new Sphere(center, 0.2, sphere_material));
+                    } else if (choose_mat < 0.95) {
+                        // metal
+                        Vector3 albedo = new Vector3(Math.random(), Math.random(), Math.random());
+                        double fuzz = random_double(0, 0.5);
+                        sphere_material = new Metal(albedo, fuzz);
+                        world.add(new Sphere(center, 0.2, sphere_material));
+                    } else {
+                        // glass
+                        sphere_material = new Dielectric(1.5);
+                        world.add(new Sphere(center, 0.2, sphere_material));
+                    }
+                }
+            }
+        }
+
+        Material material1 = new Dielectric(1.5);
+        world.add(new Sphere(new Vector3(0, 1, 0), 1.0, material1));
+
+        Material material2 = new Lambertian(new Vector3(0.4, 0.2, 0.1));
+        world.add(new Sphere(new Vector3(-4, 1, 0), 1.0, material2));
+
+        Material material3 = new Metal(new Vector3(0.7, 0.6, 0.5), 0.0);
+        world.add(new Sphere(new Vector3(4, 1, 0), 1.0, material3));
 
         Camera cam = new Camera();
 
         cam.aspect_ratio = 16.0 / 9.0;
-        cam.image_width = 800;
-        cam.samples_per_pixel = 50;
+        cam.image_width = 600;
+        cam.samples_per_pixel = 100;
         cam.max_depth = 50;
 
         cam.vfov = 20;
-        cam.lookfrom = new Vector3(-2,2,1);
-        cam.lookat = new Vector3(0,0,-1);
+        cam.lookfrom = new Vector3(13,2,3);
+        cam.lookat = new Vector3(0,0,0);
         cam.up = new Vector3(0,1,0);
 
-        //cam.defocus_angle = 10.0;
-        //cam.focus_dist = 3.4;
+        cam.defocus_angle = 0.6;
+        cam.focus_dist = 10.0;
 
         cam.render(world);
     }
